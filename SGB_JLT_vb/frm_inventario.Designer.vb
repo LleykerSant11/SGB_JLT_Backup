a45544e35de3157efabfd406ef83cf1668280e64
LleykerSant11 <lleykersantostorres890@gmail.com>
2025-11-04 17:32:51 -0500

    De nuevo

--- SGB_JLT_vb/frm_inventario.Designer.vb
+++ SGB_JLT_vb/frm_inventario.Designer.vb
@@ -38,7 +38,16 @@ Partial Class frm_inventario
         Me.label3 = New System.Windows.Forms.Label()
         Me.label4 = New System.Windows.Forms.Label()
         Me.lb_detusuario = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.IconButton2 = New FontAwesome.Sharp.IconButton()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.IconButton1 = New FontAwesome.Sharp.IconButton()
+        Me.txt_buscat = New System.Windows.Forms.TextBox()
+        Me.btn_descargarexcel = New FontAwesome.Sharp.IconButton()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
         Me.pn_container.SuspendLayout()
+        Me.Panel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'pn_container
@@ -214,17 +223,108 @@ Partial Class frm_inventario
         Me.lb_detusuario.TabIndex = 0
         Me.lb_detusuario.Text = "Detalle de Bibliotecario:"
         '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.White
+        Me.Panel1.Controls.Add(Me.IconButton2)
+        Me.Panel1.Controls.Add(Me.ComboBox1)
+        Me.Panel1.Controls.Add(Me.IconButton1)
+        Me.Panel1.Controls.Add(Me.txt_buscat)
+        Me.Panel1.Controls.Add(Me.btn_descargarexcel)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Location = New System.Drawing.Point(226, 9)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(843, 66)
+        Me.Panel1.TabIndex = 21
+        '
+        'IconButton2
+        '
+        Me.IconButton2.IconChar = FontAwesome.Sharp.IconChar.Broom
+        Me.IconButton2.IconColor = System.Drawing.Color.Black
+        Me.IconButton2.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton2.IconSize = 20
+        Me.IconButton2.Location = New System.Drawing.Point(763, 23)
+        Me.IconButton2.Name = "IconButton2"
+        Me.IconButton2.Size = New System.Drawing.Size(29, 23)
+        Me.IconButton2.TabIndex = 17
+        Me.IconButton2.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(307, 23)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 21)
+        Me.ComboBox1.TabIndex = 14
+        '
+        'IconButton1
+        '
+        Me.IconButton1.IconChar = FontAwesome.Sharp.IconChar.Search
+        Me.IconButton1.IconColor = System.Drawing.Color.Black
+        Me.IconButton1.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.IconButton1.IconSize = 20
+        Me.IconButton1.Location = New System.Drawing.Point(728, 23)
+        Me.IconButton1.Name = "IconButton1"
+        Me.IconButton1.Size = New System.Drawing.Size(29, 23)
+        Me.IconButton1.TabIndex = 16
+        Me.IconButton1.UseVisualStyleBackColor = True
+        '
+        'txt_buscat
+        '
+        Me.txt_buscat.Location = New System.Drawing.Point(434, 23)
+        Me.txt_buscat.Name = "txt_buscat"
+        Me.txt_buscat.Size = New System.Drawing.Size(288, 20)
+        Me.txt_buscat.TabIndex = 14
+        '
+        'btn_descargarexcel
+        '
+        Me.btn_descargarexcel.IconChar = FontAwesome.Sharp.IconChar.FileExcel
+        Me.btn_descargarexcel.IconColor = System.Drawing.Color.Green
+        Me.btn_descargarexcel.IconFont = FontAwesome.Sharp.IconFont.[Auto]
+        Me.btn_descargarexcel.IconSize = 20
+        Me.btn_descargarexcel.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.btn_descargarexcel.Location = New System.Drawing.Point(7, 26)
+        Me.btn_descargarexcel.Name = "btn_descargarexcel"
+        Me.btn_descargarexcel.Size = New System.Drawing.Size(130, 28)
+        Me.btn_descargarexcel.TabIndex = 14
+        Me.btn_descargarexcel.Text = "Descargar Excel"
+        Me.btn_descargarexcel.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.btn_descargarexcel.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(234, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(64, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Busacr por: "
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("MS UI Gothic", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(3, 3)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(245, 19)
+        Me.Label6.TabIndex = 0
+        Me.Label6.Text = "Listado de M.Bibliografico:"
+        '
         'frm_inventario
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(4, Byte), Integer), CType(CType(132, Byte), Integer), CType(CType(36, Byte), Integer))
-        Me.ClientSize = New System.Drawing.Size(1051, 495)
+        Me.ClientSize = New System.Drawing.Size(1081, 495)
+        Me.Controls.Add(Me.Panel1)
         Me.Controls.Add(Me.pn_container)
         Me.Name = "frm_inventario"
         Me.Text = "frm_inventario"
         Me.pn_container.ResumeLayout(False)
         Me.pn_container.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -245,4 +345,12 @@ Partial Class frm_inventario
     Private WithEvents label3 As Label
     Private WithEvents label4 As Label
     Private WithEvents lb_detusuario As Label
+    Private WithEvents Panel1 As Panel
+    Friend WithEvents IconButton2 As FontAwesome.Sharp.IconButton
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents IconButton1 As FontAwesome.Sharp.IconButton
+    Friend WithEvents txt_buscat As TextBox
+    Friend WithEvents btn_descargarexcel As FontAwesome.Sharp.IconButton
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label6 As Label
 End Class
